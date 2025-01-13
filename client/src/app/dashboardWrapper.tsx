8cb7178efced50159900fe884879c7f18d030371
Meet-Patel-123 <meet.patel@cognitivecoreai.com>
2025-01-13 17:55:43 +0530

    sidebar

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
 
 const DashboardWrapper = ({ children }: { childern: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      {/* Sidebar */}
+      <Sidebar />
       Sidebar
       <main className="flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64">
         <Navbar />
